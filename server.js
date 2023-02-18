d4ea73e3c6c85ecb579c36084ab8c01933483783
Jonathan C Dela Peña <jonathancastillondelapena28@gmail.com>
2023-02-18 13:24:23 +0800

    Implemented(partial): Connection to MongoDB

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
+const dotEnv = require("dotenv");
 
+dotEnv.config();
 const app = express();
 
 const corsOptions = {
@@ -12,13 +14,24 @@ app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// test route
-app.get("/", (req, res) => {
-  res.json({ message: "Test Route..." });
-});
+const testRoutes = require("./src/routes/testRoutes");
+app.use(testRoutes);
+// require("./src/routes/testRoutes")(app);
 
 // set PORT to listen for requests
 const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
+
+// set connection to MongoDB
+const db = require("./src/data/database");
+db.mongoose
+  .connect(db.connectionString)
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.log("Cannot connect to MongoDB...", err);
+    process.exit();
+  });
