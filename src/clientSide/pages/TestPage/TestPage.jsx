9744d8d8ccc9bc03493e1213d757ad37b02ac815
Jonathan C Dela Peña <jonathancastillondelapena28@gmail.com>
2023-02-18 19:08:33 +0800

    Implemented(partial, no validation): MERN Test page that can only post new test record

---
+++ src/clientSide/pages/TestPage/TestPage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import CreateTestRecord from "./components/CreateTestRecord";
+
+const TestPage = () => {
+  return (
+    <div className="container">
+      <CreateTestRecord />
+    </div>
+  );
+};
+
+export default TestPage;
