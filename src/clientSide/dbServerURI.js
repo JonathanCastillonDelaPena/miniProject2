1c0f71a59ff478133596bc2c963223a796c09951
Jonathan C Dela Peña <jonathancastillondelapena28@gmail.com>
2023-02-20 17:57:37 +0800

    Restructured the folders, completely moved the back-end to a different repository; The front-end can now connect to a deployed db server

---
+++ src/clientSide/dbServerURI.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+// Use this for local testing
+export default axios.create({ baseURL: "http://localhost:8081" });
+
+// Deployed database server
+// export default axios.create({
+//   baseURL: "https://mini-project-2-server.cyclic.app",
+// });
